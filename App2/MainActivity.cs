defd2336ab03419492c61e5660905b4b4eeb338b
Robinson Campo Zambrano <robinson.campo@outlook.com>
2017-09-05 00:06:39 -0500

    Chambios

--- App2/MainActivity.cs
+++ App2/MainActivity.cs
@@ -10,7 +10,7 @@ namespace App2
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            // Este es un comentario para ver si hay algun cambio pendiente
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
         }
